c1798b40f080f8fb10be60b73d814e2a66c822d8
Athroniaeth <pierre.chaumont@hotmail.fr>
2025-10-07 21:59:04 +0200

    docs(example): corrects bad typing of protected route

--- examples/example_fastapi.py
+++ examples/example_fastapi.py
@@ -3,7 +3,7 @@ import os
 from fastapi import FastAPI, Depends, APIRouter
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-from fastapi_api_key import Argon2ApiKeyHasher
+from fastapi_api_key import Argon2ApiKeyHasher, ApiKey
 from fastapi_api_key.router import create_api_keys_router, create_api_key_security
 
 # Create the async engine and session maker
@@ -22,8 +22,19 @@ router_protected = APIRouter(prefix="/protected", tags=["Protected"])
 
 
 @router_protected.get("/")
-async def read_protected_data(api_key: str = Depends(security)):
-    return {"message": "This is protected data", "api_key": api_key}
+async def read_protected_data(api_key: ApiKey = Depends(security)):
+    return {
+        "message": "This is protected data",
+        "apiKey": {
+            "id": api_key.id_,
+            "name": api_key.name,
+            "description": api_key.description,
+            "isActive": api_key.is_active,
+            "createdAt": api_key.created_at,
+            "expiresAt": api_key.expires_at,
+            "lastUsedAt": api_key.last_used_at,
+        },
+    }
 
 
 router_api_keys = create_api_keys_router(async_session_maker, hasher=hasher, router=router)
